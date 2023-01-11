22b9e4627c80b92f23c4c3fe2a09f99b53d7e5fe
Zachary Laguna <lagunazachary@gmail.com>
2023-01-10 21:06:13 -0600

    update projects

--- src/app/projects/projects.component.ts
+++ src/app/projects/projects.component.ts
@@ -17,9 +17,21 @@ export class ProjectsComponent implements OnInit {
   goToLyric() {
     this.document.location.href = 'http://3.20.71.118:5000/';
   }
+  
+  goToLyricCode() {
+    this.document.location.href = 'https://github.com/zacharylaguna/lyric-retriever';
+  }
 
   goToBaby() {
     this.document.location.href = 'https://drive.google.com/drive/folders/11k6lYF7ScDIxzzvbVaWI3ZNNwwv0Py6N?usp=sharing';
+  }
+
+  goToRealMgmt() {
+    this.document.location.href = 'http://3.17.248.106:3000/';
+  }
+
+  goToRealMgmtCode() {
+    this.document.location.href = 'https://github.com/zacharylaguna/real-mgmt';
   }
 
 }
